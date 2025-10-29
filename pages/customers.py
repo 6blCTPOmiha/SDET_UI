bc800027817a0016d048a7d0efd9464621b19ffa
6blCTPOmiha <bystrovmiha@gmail.com>
2025-10-29 11:01:44 +0400

    В импортах добавил статический ксс запрос для клиентов и вспомогательные функции
    Добавил 2 функции:
    - Нажатие на строку First Name для сортировки списка клиентов
    - Удаление необходимого клиента по заданию (клиент у которого длина имени максимально приближена к средней длине имён всех клиентов)

--- pages/customers.py
+++ pages/customers.py
@@ -1,4 +1,5 @@
-from data.text_data import URL
+from data.text_data import URL, CUSTOMERS_MAIN_DATA_CSS
+from helpers.help_func import StaticFunctions as Sf
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -30,3 +31,18 @@ class Customers:
         wait.until(EC.presence_of_element_located(
             (By.XPATH, '//td[contains(text(), "Hermoine")]')
         ), message='Форма не загружается')
+
+
+    def click_first_name(self):
+        first_name = self.driver.find_element(By.XPATH, '//a[contains(text(), "First Name")]')
+        first_name.click()
+
+
+    def delete_close_name(self):
+        customers = self.driver.find_elements(By.CSS_SELECTOR, CUSTOMERS_MAIN_DATA_CSS)
+        names = Sf.chunks_to_names(customers)
+        sr_ar = Sf.math_round(Sf.calc_sr_ar(names))
+        del_id = Sf.calc_mid_len_name(names, sr_ar)
+        del_btns = self.driver.find_elements(By.CSS_SELECTOR, '[ng-click="deleteCust(cust)"]')
+        del_btns[del_id].click()
+
