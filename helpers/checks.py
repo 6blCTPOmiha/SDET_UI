9c52d45686fd7a19965623cfe04298ba7100d87a
6blCTPOmiha <bystrovmiha@gmail.com>
2025-10-31 14:53:21 +0400

    Шаги allure переведены на русский язык Добавлены русские комментарии ко всем Assert error'ам

--- helpers/checks.py
+++ helpers/checks.py
@@ -13,39 +13,39 @@ class Checks:
 
     def add_customer_form_loaded(self):
         customer = self.driver.find_element(By.CSS_SELECTOR, '[type = "submit"]')
-        with allure.step('Check if add_customer form is loaded'):
-            assert customer != ''
+        with allure.step('Проверка загрузки формы add_customer'):
+            assert customer != '', 'Форма add customer не загружается'
 
 
     def customers_form_loaded(self):
         customer = self.driver.find_element(By.XPATH, '//td[contains(text(), "Hermoine")]')
-        with allure.step('Check if customers form is loaded'):
-            assert customer != ''
+        with allure.step('Проверка загрузки формы customers'):
+            assert customer != '', 'Форма customers не загружается'
 
 
     def new_one_customer_added(self):
         customers = self.driver.find_elements(By.CSS_SELECTOR, CUSTOMERS_MAIN_DATA_CSS)
         customer_count = len(customers) // 3
-        with allure.step('Check customers count'):
-            assert customer_count == 6
+        with allure.step('Проверка количества клиентов'):
+            assert customer_count == 6, 'Количество пользователей некорректное'
 
 
     def first_name_reverse_sort(self):
         customers = self.driver.find_elements(By.CSS_SELECTOR, CUSTOMERS_MAIN_DATA_CSS)
         names = Sf.chunks_to_names(customers)
-        with allure.step('Check customers reverse sort'):
-            assert names == sorted(names, reverse=True)
+        with allure.step('Проверка обратной сортировки клиентов'):
+            assert names == sorted(names, reverse=True), 'Сортировка выполнена некорректно'
 
 
     def first_name_sort(self):
         customers = self.driver.find_elements(By.CSS_SELECTOR, CUSTOMERS_MAIN_DATA_CSS)
         names = Sf.chunks_to_names(customers)
-        with allure.step('Check customers sort'):
-            assert names == sorted(names)
+        with allure.step('Проверка сортировки клиентов'):
+            assert names == sorted(names), 'Сортировка выполнена некорректно'
 
 
     def delete_customer_check(self):
         customers = self.driver.find_elements(By.CSS_SELECTOR, CUSTOMERS_MAIN_DATA_CSS)
         names = Sf.chunks_to_names(customers)
-        with allure.step('Check deleted customer in customers list'):
-            assert 'Harry' not in names
+        with allure.step('Проверка удаления клиента из списка'):
+            assert 'Harry' not in names, 'удалён некорректный клиент'
