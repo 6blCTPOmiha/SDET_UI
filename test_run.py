41f316f2997f3e1207ff1066b6aa3cfcd4f9f191
6blCTPOmiha <bystrovmiha@gmail.com>
2025-11-01 16:09:40 +0400

    Убрал лишнее из тест-кейсов + занёс выбор формы в предусловие

--- test_run.py
+++ test_run.py
@@ -20,35 +20,35 @@ def test_positive_add_customer(driver):
     #  Ожидание загрузки страницы
     add_customer.wait_for_page_load()
 
-    #  Шаг 1 : Нажимаем на "Add customer"
+    #  Предусловие 3 : Нажимаем на "Add customer"
     add_customer.click_add_customer()
 
     #  Ожидание загрузки формы
     add_customer.wait_for_form_load()
 
-    #  Шаг 2 : Проверяем загрузку формы
+    #  Проверяем загрузку формы
     checks.add_customer_form_loaded()
 
-    #  Шаг 3 : Вводим имя
+    #  Шаг 1 : Вводим имя
     add_customer.input_first_name()
 
-    #  Шаг 4 : Вводим фамилию
+    #  Шаг 2 : Вводим фамилию
     add_customer.input_last_name()
 
-    #  Шаг 5 : Вводим код
+    #  Шаг 3 : Вводим код
     add_customer.input_post_code()
 
-    #  Шаг 6 : Нажимаем на кнопку подтверждения введённых данных
+    #  Шаг 4 : Нажимаем на кнопку подтверждения введённых данных
     add_customer.click_add_customer_button()
 
-    #  Шаг 7 : Нажимаем ОК в уведомлении
+    #  Шаг 5 : Нажимаем ОК в уведомлении
     add_customer.ok_alert_message()
 
-    #  Шаг 8 : Нажимаем на кнопку "Customers" (переходим к списку пользователей)
+    #  Шаг 6 : Нажимаем на кнопку "Customers" (переходим к списку пользователей)
     customers.click_customers()
     customers.wait_for_form_load()
 
-    #  Шаг 9 : Проверяем количество пользователей
+    #  Шаг 7 : Проверяем количество пользователей
     checks.new_one_customer_added()
 
 
@@ -66,25 +66,25 @@ def test_positive_customers_name_sort(driver):
     #  Ожидание загрузки страницы
     customers.wait_for_page_load()
 
-    #  Шаг 1 : Нажимаем на "Customers"
+    #  Предусловие 3 : Нажимаем на "Customers"
     customers.click_customers()
 
     #  Ожидание загрузки формы
     customers.wait_for_form_load()
 
-    #  Шаг 2 : Проверяем загрузку формы
+    #  Проверяем загрузку формы
     checks.customers_form_loaded()
 
-    #  Шаг 3 : Кликаем на "First Name"
+    #  Шаг 1 : Кликаем на "First Name"
     customers.click_first_name()
 
-    #  Шаг 4 : Проверяем на обратную сортировку
+    #  Шаг 2 : Проверяем на обратную сортировку
     checks.first_name_reverse_sort()
 
-    #  Шаг 5 : Снова кликаем на "First Name"
+    #  Шаг 3 : Снова кликаем на "First Name"
     customers.click_first_name()
 
-    #  Шаг 6 : Проверяем на сортировку
+    #  Шаг 4 : Проверяем на сортировку
     checks.first_name_sort()
 
 
@@ -101,19 +101,19 @@ def test_positive_customer_delete(driver):
     #  Ожидание загрузки страницы
     customers.wait_for_page_load()
 
-    #  Шаг 1 : Нажимаем на "Customers"
+    #  Предусловие 3 : Нажимаем на "Customers"
     customers.click_customers()
 
     #  Ожидание загрузки формы
     customers.wait_for_form_load()
 
-    #  Шаг 2 : Проверяем загрузку формы
+    #  Проверяем загрузку формы
     checks.customers_form_loaded()
 
-    #  Шаг 3 : Удаляем нужного клиента
+    #  Шаг 1 : Удаляем нужного клиента
     customers.delete_close_name()
 
-    #  Шаг 4 : Проверяем, удалился ли нужный клиент
+    #  Шаг 2 : Проверяем, удалился ли нужный клиент
     checks.delete_customer_check()
 
 
@@ -132,29 +132,29 @@ def test_negative_add_customer(driver):
     #  Ожидание загрузки страницы
     add_customer.wait_for_page_load()
 
-    #  Шаг 1 : Нажимаем на "Add customer"
+    #  Предусловие 3 : Нажимаем на "Add customer"
     add_customer.click_add_customer()
 
     #  Ожидание загрузки формы
     add_customer.wait_for_form_load()
 
-    #  Шаг 2 : Проверяем загрузку формы
+    #  Проверяем загрузку формы
     checks.add_customer_form_loaded()
 
-    #  Шаг 3 : Вводим имя
+    #  Шаг 1 : Вводим имя
     add_customer.input_first_name()
 
-    #  Шаг 4 : Вводим фамилию
+    #  Шаг 2 : Вводим фамилию
     add_customer.input_last_name()
 
-    #  Шаг 5 : Вводим код
+    #  Шаг 3 : Вводим код
     add_customer.input_post_code()
 
-    #  Шаг 6 : Нажимаем на кнопку "Customers" (переходим к списку пользователей)
+    #  Шаг 4 : Нажимаем на кнопку "Customers" (переходим к списку пользователей)
     customers.click_customers()
     customers.wait_for_form_load()
 
-    #  Шаг 7 : Проверяем количество пользователей
+    #  Шаг 5 : Проверяем количество пользователей
     checks.new_one_customer_added()
 
 
@@ -171,19 +171,19 @@ def test_negative_customers_name_sort(driver):
     #  Ожидание загрузки страницы
     customers.wait_for_page_load()
 
-    #  Шаг 1 : Нажимаем на "Customers"
+    #  Предусловие 3 : Нажимаем на "Customers"
     customers.click_customers()
 
     #  Ожидание загрузки формы
     customers.wait_for_form_load()
 
-    #  Шаг 2 : Проверяем загрузку формы
+    #  Проверяем загрузку формы
     checks.customers_form_loaded()
 
-    #  Шаг 3 : Кликаем на "First Name"
+    #  Шаг 1 : Кликаем на "First Name"
     customers.click_first_name()
 
-    #  Шаг 4 : Проверяем на сортировку
+    #  Шаг 2 : Проверяем на сортировку
     checks.first_name_sort()
 
 
@@ -200,17 +200,17 @@ def test_negative_customer_delete(driver):
     #  Ожидание загрузки страницы
     customers.wait_for_page_load()
 
-    #  Шаг 1 : Нажимаем на "Customers"
+    #  Предусловие 3 : Нажимаем на "Customers"
     customers.click_customers()
 
     #  Ожидание загрузки формы
     customers.wait_for_form_load()
 
-    #  Шаг 2 : Проверяем загрузку формы
+    #  Проверяем загрузку формы
     checks.customers_form_loaded()
 
-    #  Шаг 3 : Удаляем клиента, который не подходит под условие
+    #  Шаг 1 : Удаляем клиента, который не подходит под условие
     customers.delete_wrong_name()
 
-    #  Шаг 4 : Проверяем, удалился ли нужный клиент
+    #  Шаг 2 : Проверяем, удалился ли нужный клиент
     checks.delete_customer_check()
