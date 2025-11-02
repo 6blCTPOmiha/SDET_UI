d2e948504cd52b80d44d7c06135d433765255ce3
6blCTPOmiha <bystrovmiha@gmail.com>
2025-11-02 17:52:36 +0400

    Здесь и далее: избыточные комментарии. Не нужно комментировать каждую строчку. Комментарии используются для описания/пояснения неявного участка кода
    Решено

--- test_run.py
+++ test_run.py
@@ -9,46 +9,22 @@ from helpers.checks import Checks
 @allure.story('Тестирование добавления нового пользователя')
 @pytest.mark.add_customer
 def test_positive_add_customer(driver):
-    #  Предусловие 1 : Открыть браузер
+
     add_customer = AddCustomer(driver)
     customers = Customers(driver)
     checks = Checks(driver)
-
-    #  Предусловие 2 : Перейти по ссылке
     add_customer.open()
-
-    #  Ожидание загрузки страницы
     add_customer.wait_for_page_load()
-
-    #  Предусловие 3 : Нажимаем на "Add customer"
     add_customer.click_add_customer()
-
-    #  Ожидание загрузки формы
     add_customer.wait_for_form_load()
-
-    #  Проверяем загрузку формы
     checks.add_customer_form_loaded()
-
-    #  Шаг 1 : Вводим имя
     add_customer.input_first_name()
-
-    #  Шаг 2 : Вводим фамилию
     add_customer.input_last_name()
-
-    #  Шаг 3 : Вводим код
     add_customer.input_post_code()
-
-    #  Шаг 4 : Нажимаем на кнопку подтверждения введённых данных
     add_customer.click_add_customer_button()
-
-    #  Шаг 5 : Нажимаем ОК в уведомлении
     add_customer.ok_alert_message()
-
-    #  Шаг 6 : Нажимаем на кнопку "Customers" (переходим к списку пользователей)
     customers.click_customers()
     customers.wait_for_form_load()
-
-    #  Шаг 7 : Проверяем количество пользователей
     checks.new_one_customer_added()
 
 
@@ -56,64 +32,32 @@ def test_positive_add_customer(driver):
 @allure.story('Тестирование сортировки в 2 стороны')
 @pytest.mark.name_sort
 def test_positive_customers_name_sort(driver):
-    #  Предусловие 1 : Открыть браузер
+
     customers = Customers(driver)
     checks = Checks(driver)
-
-    #  Предусловие 2 : Перейти по ссылке
     customers.open()
-
-    #  Ожидание загрузки страницы
     customers.wait_for_page_load()
-
-    #  Предусловие 3 : Нажимаем на "Customers"
     customers.click_customers()
-
-    #  Ожидание загрузки формы
     customers.wait_for_form_load()
-
-    #  Проверяем загрузку формы
     checks.customers_form_loaded()
-
-    #  Шаг 1 : Кликаем на "First Name"
     customers.click_first_name()
-
-    #  Шаг 2 : Проверяем на обратную сортировку
     checks.first_name_reverse_sort()
-
-    #  Шаг 3 : Снова кликаем на "First Name"
     customers.click_first_name()
-
-    #  Шаг 4 : Проверяем на сортировку
     checks.first_name_sort()
 
 
 @allure.story('Тестирование удаления пользователя')
 @pytest.mark.delete
 def test_positive_customer_delete(driver):
-    #  Предусловие 1 : Открыть браузер
+
     customers = Customers(driver)
     checks = Checks(driver)
-
-    #  Предусловие 2 : Перейти по ссылке
     customers.open()
-
-    #  Ожидание загрузки страницы
     customers.wait_for_page_load()
-
-    #  Предусловие 3 : Нажимаем на "Customers"
     customers.click_customers()
-
-    #  Ожидание загрузки формы
     customers.wait_for_form_load()
-
-    #  Проверяем загрузку формы
     checks.customers_form_loaded()
-
-    #  Шаг 1 : Удаляем нужного клиента
     customers.delete_close_name()
-
-    #  Шаг 2 : Проверяем, удалился ли нужный клиент
     checks.delete_customer_check()
 
 
@@ -121,96 +65,47 @@ def test_positive_customer_delete(driver):
 @pytest.mark.add_customer
 def test_negative_add_customer(driver):
 
-    #  Предусловие 1 : Открыть браузер
     add_customer = AddCustomer(driver)
     customers = Customers(driver)
     checks = Checks(driver)
-
-    #  Предусловие 2 : Перейти по ссылке
     add_customer.open()
-
-    #  Ожидание загрузки страницы
     add_customer.wait_for_page_load()
-
-    #  Предусловие 3 : Нажимаем на "Add customer"
     add_customer.click_add_customer()
-
-    #  Ожидание загрузки формы
     add_customer.wait_for_form_load()
-
-    #  Проверяем загрузку формы
     checks.add_customer_form_loaded()
-
-    #  Шаг 1 : Вводим имя
     add_customer.input_first_name()
-
-    #  Шаг 2 : Вводим фамилию
     add_customer.input_last_name()
-
-    #  Шаг 3 : Вводим код
     add_customer.input_post_code()
-
-    #  Шаг 4 : Нажимаем на кнопку "Customers" (переходим к списку пользователей)
     customers.click_customers()
     customers.wait_for_form_load()
-
-    #  Шаг 5 : Проверяем количество пользователей
     checks.new_one_customer_added()
 
 
 @allure.story('Тестирование сортировки в 2 стороны')
 @pytest.mark.name_sort
 def test_negative_customers_name_sort(driver):
-    #  Предусловие 1 : Открыть браузер
+
     customers = Customers(driver)
     checks = Checks(driver)
-
-    #  Предусловие 2 : Перейти по ссылке
     customers.open()
-
-    #  Ожидание загрузки страницы
     customers.wait_for_page_load()
-
-    #  Предусловие 3 : Нажимаем на "Customers"
     customers.click_customers()
-
-    #  Ожидание загрузки формы
     customers.wait_for_form_load()
-
-    #  Проверяем загрузку формы
     checks.customers_form_loaded()
-
-    #  Шаг 1 : Кликаем на "First Name"
     customers.click_first_name()
-
-    #  Шаг 2 : Проверяем на сортировку
     checks.first_name_sort()
 
 
 @allure.story('Тестирование удаления пользователя')
 @pytest.mark.delete
 def test_negative_customer_delete(driver):
-    #  Предусловие 1 : Открыть браузер
+
     customers = Customers(driver)
     checks = Checks(driver)
-
-    #  Предусловие 2 : Перейти по ссылке
     customers.open()
-
-    #  Ожидание загрузки страницы
     customers.wait_for_page_load()
-
-    #  Предусловие 3 : Нажимаем на "Customers"
     customers.click_customers()
-
-    #  Ожидание загрузки формы
     customers.wait_for_form_load()
-
-    #  Проверяем загрузку формы
     checks.customers_form_loaded()
-
-    #  Шаг 1 : Удаляем клиента, который не подходит под условие
     customers.delete_wrong_name()
-
-    #  Шаг 2 : Проверяем, удалился ли нужный клиент
     checks.delete_customer_check()
