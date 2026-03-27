510842db23d6880124ac1a63cf052bb403df2031
Mike <58184445+6blCTPOmiha@users.noreply.github.com>
2026-03-28 02:31:41 +0400

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -9,6 +9,7 @@ def driver():
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
+    options.add_argument("--headless=new")
     options.page_load_strategy = 'eager'
     options.set_capability("browserName", "chrome")
     driver = webdriver.Remote(command_executor='http://selenoid:4444/wd/hub', options=options)
