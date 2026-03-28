cc62ecd7d435e429a31719b2f63bcc938a907e21
6blCTPOmiha <bystrovmiha@gmail.com>
2026-03-28 14:23:41 +0400

    Чек на коммит из пайчарма

--- conftest.py
+++ conftest.py
@@ -6,13 +6,15 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture
 def driver():
     options = Options()
-    # options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
+    options.add_argument("--headless=new")
     options.page_load_strategy = 'eager'
-    # driver = webdriver.Remote(command_executor='http://selenoid:4444/wd/hub', options=options)
-    driver = webdriver.Chrome(options=options)
+    options.set_capability("browserName", "chrome")
+    driver = webdriver.Remote(command_executor='http://selenoid:4444/wd/hub', options=options)
+    # driver = webdriver.Chrome(options=options)
     driver.maximize_window()
     yield driver
     driver.quit()
+
